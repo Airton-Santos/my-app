849675ead0425fb0841e53eafd38908d1c5368cd
Airton J <santosairton31508@gmail.com>
2024-10-14 14:27:45 -0300

    Modificação no background do app

--- App.js
+++ App.js
@@ -17,11 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bf2424',
     alignItems: 'center',
     justifyContent: 'center',
   },
   texto: {
-    
+    color: '#fff'
   }
 });
