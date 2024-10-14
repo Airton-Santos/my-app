ee8eba3e99921a2b8e6b7e2bba5a360b185dba1b
Airton J <santosairton31508@gmail.com>
2024-10-14 15:58:34 -0300

    Adicionado Tela de Cadastro

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import TextInputAffix from 'react-native-paper/lib/typescript/components/TextInput/Adornment/TextInputAffix';
 import TextInputIcon from 'react-native-paper/lib/typescript/components/TextInput/Adornment/TextInputIcon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Hello World!</Text>
-      <TextInput/>
+      <Text style={styles.texto}>Cadastro:</Text>
+      <TextInput
+        label="Email"
+        value={ textEmail }        
+      />
       <StatusBar style="auto" />
     </View>
   );
