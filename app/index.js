a6145ef3707ea2e548777cc3ac080d3dc9925d57
Airton J <santosairton31508@gmail.com>
2024-10-31 16:22:00 -0300

    Implementação da api na lista de comidas

--- app/index.js
+++ app/index.js
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Link } from 'expo-router'
-import React from 'react'
-import { Button } from 'react-native-paper'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Link, router } from 'expo-router'
+import React, { useEffect, useState } from 'react'
+import { Button, List } from 'react-native-paper'
+
+const Comida = ({descricao, valor}) => {
+  return (
+    <List.Item title = {descricao} description = {valor}/>
+  );
+};
 
 const Index = () => {
+
+  const [comidas, setComidas] =  useState("")
+
+  const getComidas = async () => {
+    console.log("getComidas");
+
+    const response = await fetch('https://66fc26cec3a184a84d1642a4.mockapi.io/Lanches');
+    const json = await response.json();
+    setComidas(json)
+  };
+
+  useEffect(()=>{
+    getComidas(); 
+  });
+
   return (
     <View>
-      <Text>Index</Text>
+      <FlatList data = {comidas} renderItem = {({ item }) => <Comida descricao={item.descricao} valor={item.valor} />} />
+
       <Link href='/cadastro' asChild>
         <Button mode='contained'>Novo Lanche</Button>
       </Link>
